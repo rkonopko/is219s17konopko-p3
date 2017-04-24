9bd52629eb5ac641dfaf0a29ad19962aedbe3dfe
rkonopko <konopkor@gmail.com>
2017-04-24 19:05:52 -0400

    posterClick and timeText added
    The function poster click increments the poster images and loops back around after the last image. The function timeText takes the length of the movie, which is in minutes, and converts it into hours and minutes.

--- HomeController.js
+++ HomeController.js
@@ -107,16 +107,30 @@ app.controller('HomeController', ['$scope', function($scope) {
 	/* ADD FUNCTIONS FOR STEP 7 HERE */
 		
 	$scope.like = function(index) {
-        
             $scope.movies[index].likes++;
-            
-        };
+      };
+      
       $scope.dislike = function(index) {
-        
-            $scope.movies[index].dislikes--;
-            
-        };
+            $scope.movies[index].dislikes--      
+      };
 	
+      $scope.posterClick = function(index) 
+      {
+      
+      if ($scope.movies[index].posterindex > $scope.movies[index].posters.length - 1) {
+            $scope.movies[index].posterindex = 0;
+      
+      } else {
+            $scope.movies[index].posterindex++;
+      }
+      
+      $scope.timeText= function(minutes)
+      {
+            $scope.hours = (Math.floor(Math.abs(minutes) / 60));  
+            $scope.minutes = (Math.abs(minutes) % 60);  
+            return  $scope.hours +'hrs '+$scope.minutes + 'min';  
+      }; 
 
+    };
 
 }]);
